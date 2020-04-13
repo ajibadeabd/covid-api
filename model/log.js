03d1e5fd16de506f1d2abf8cfd8c75abb89c9ab4
ajibadeabd <ajibadeabd@gmail.com>
2020-04-13 08:45:59 +0100

    deploying to heroku

--- model/log.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const { Schema } = mongoose;
-const logSchema = new Schema({
-  log: String
-});
-
-module.exports = mongoose.model('log', logSchema);
--- route/api.js
+++ route/api.js
@@ -1,15 +1,10 @@
 const sf = require('slice-file')
 const express = require('express');
 const router = express.Router();
-const Covid = require('../model/covid');
 const covid = require('../helper/covid');
 const fs = require('fs');
 const convert = require('xml-js')
 const path = require('path')
-// const xmlparser = require('express-xml-bodyparser');
-
-
-
 
 router.get('/', (req, res, next) => {
   res.send('<h1>api-covid</h1>');
@@ -18,20 +13,12 @@ router.get('/', (req, res, next) => {
 
 
 router.post('/v1/on-covid-19', (req, res, next) => {
- let input = req.body;
-    //  covid(input);
-  console.log(covid(input));
-    const newCovid = (covid(input));
-    // console.log(input.region.avgDailyIncomeInUSD)
-    // console.log(input.region.avgDailyIncomePopulation)
-    let nw = new Covid(newCovid)
-    nw.save()
-    .then( covidResult=>{
-        res.json(
-            covidResult
-         )
-    })
-    
+  let input = req.body;
+       //  covid(input);
+     console.log(covid(input));
+       const newCovid = (covid(input));
+     res.status(200).json(newCovid)
+       
 
 });
 
@@ -40,39 +27,20 @@ router.post('/v1/on-covid-19/json', (req, res, next) => {
        //  covid(input);
      console.log(covid(input));
        const newCovid = (covid(input));
-      
-       let nw = new Covid(newCovid)
-       nw.save()
-       .then( covidResult=>{
-           res.json(
-               covidResult
-            )
-       })
+     res.status(200).json(newCovid)
        
    
    });
 router.post('/v1/on-covid-19/xml', (req, res, next) => {
    let  options = {compact: true, ignoreComment: true, spaces:4}
-    let input = req.body;
-    const json_xml = input;
-   let xml= convert.json2xml(json_xml,options)
-  //  console.log(xml)
-  const newCovid = (covid(xml));
-
-
-     let nw = new Covid(newCovid)
-     nw.save()
-.then(covidResult=>{
-   console.log(covidResult)
-
-   res.status(200).type('application/xml').send(covidResult);
-})
-    
+   let input = req.body;
+   const newCovid = (covid(input));
+   let xml= convert.json2xml(newCovid,options)  
+   res.status(200).type('application/xml').send(xml);
 
 });
 
 router.get('/v1/on-covid-19/logs', (req, res, next) => {
-    // const data = fs.readFileSync(( 'db/access.log'))
     const resData = [];
     const filename = sf('db/access.log')
     filename.sliceReverse().on('data',(data)=>{
